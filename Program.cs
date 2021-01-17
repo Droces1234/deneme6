ef559d221be430336fbe3563c426513f9bcd14a6
Droces1234 <ebubekircicek1234@gmail.com>
2021-01-17 16:58:23 +0300

    project pertama

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, sonuc;
+            Console.Write("A değeri: ");
+            a = Convert.ToInt16(Console.ReadLine());
+            Console.Write("B değeri: ");
+            b = Convert.ToInt16(Console.ReadLine());
+            Console.Write("C değeri: ");
+            c = Convert.ToInt16(Console.ReadLine());
+            Console.Write("D değeri: ");
+            d = Convert.ToInt16(Console.ReadLine());
+            sonuc = Math.Abs((a * d) - (b * c));
+            Console.Write("determinant: " + sonuc);
+            Console.ReadLine();
+        } 
+    }
+}
